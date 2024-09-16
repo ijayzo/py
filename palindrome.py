0bdc7cc44a16713125b788fc53a6819b18319ca7
ijayzo <97761169+ijayzo@users.noreply.github.com>
2024-09-16 13:43:02 -0500

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,3 +1,20 @@
+def palindromes(word: str):
+    for i in range(len(word)//2):
+        if word[i] != word[len(word)-i-1]:
+            return False
+    return True
+ 
+while True:
+    word = input("Please type in a palindrome: ")
+    if palindromes(word):
+        print(word, "is a palindrome!")
+        break
+    print("that wasn't a palindrome")
+
+
+---
+# or 
+
 def palindromes(str1) :
     return str1 == str1[::-1]
 str1 = "  "
