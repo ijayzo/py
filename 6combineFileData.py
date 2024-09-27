bc5335a64d799dc4170bc234bcd31b26b91563a1
ijayzo <97761169+ijayzo@users.noreply.github.com>
2024-09-27 13:55:09 -0500

    Update 6combineFileData.py

--- 6combineFileData.py
+++ 6combineFileData.py
@@ -1,28 +1,3 @@
-student_data = input("Student information: ")
-exercise_data = input("Exercises completed: ")
- 
-students = {}
-with open(student_data) as file:
-    for row in file:
-        parts = row.split(";")
-        if parts[0] == "id":
-            continue
-        students[parts[0]] = f"{parts[1]} {parts[2].strip()}"
- 
-exercises = {}
-with open(exercise_data) as file:
-    for row in file:
-        parts = row.split(";")
-        if parts[0] == "id":
-            continue
-        esum = 0
-        for i in range(1, 8):
-            esum += int(parts[i])
-        exercises[parts[0]] = esum
- 
-for student_id, name in students.items():
-    print(f"{name} {exercises[student_id]}")
-```
 # write your solution here
 if True:
     # this is never executed
@@ -37,11 +12,10 @@ names = {}
 
 with open(student_info) as new_file:
     for line in new_file:
-        line = line.replace("\n", "")
         parts = line.split(';')
         if parts[0] == "id":
             continue
-        names[parts[0]] = parts[1] + " " + parts[2]
+        names[parts[0]] = parts[1] + " " + parts[2].strip()
 
 grades = {}
 
@@ -52,7 +26,6 @@ with open(exercise_data) as new_file:
             continue
         s = 0
         for part in parts[1 : ] :
-            #print(part)
             s += int(part)
         grades[parts[0]] = s 
 
@@ -60,4 +33,4 @@ for id, names  in names.items():
     if id in grades:
         grade = grades[id]
         print(f"{names} {grade}")
-```
+
